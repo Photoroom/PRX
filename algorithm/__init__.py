75289b5f5e313752fe0b66ff38d921dfde0da96b
David Bertoin <davidbertoin@gmail.com>
2026-02-19 16:47:56 +0000

    Add perceptual Dino algorithm

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -3,6 +3,7 @@ from algorithm.repa import REPA
 from algorithm.tread import Tread
 from algorithm.sprint import SPRINT
 from algorithm.contrastive_flow_matching import ContrastiveFlowMatching
+from algorithm.perceptual_dino import PerceptualDINO
 
 
-__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching"]
+__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching", "PerceptualDINO"]
