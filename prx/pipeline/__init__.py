30f9340f8c6b376a390c4b893901568ec0efe742
David Bertoin <davidbertoin@gmail.com>
2026-03-05 15:11:28 +0000

    remove backward compatibility insurrance

--- prx/pipeline/__init__.py
+++ prx/pipeline/__init__.py
@@ -1,11 +1,10 @@
 from .fm_pipeline import EMAModel, FMPipeline, ModelInputs
-from .composer_pipeline import ComposerFMPipeline, Pipeline
+from .composer_pipeline import ComposerFMPipeline
 from .models_factory import build_pipeline, build_schedulers, wrap_fsdp_module, resolve_torch_dtype
 
 __all__ = [
     "FMPipeline",
     "ComposerFMPipeline",
-    "Pipeline",
     "EMAModel",
     "ModelInputs",
     "build_pipeline",
